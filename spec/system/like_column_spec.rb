4c5164a816c40f39773e95115d5ec8e4ede2ad1f
David Taylor <david@taylorhq.com>
2025-01-13 16:49:57 +0000

    DEV: Update for glimmer topic-list

---
+++ spec/system/like_column_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+RSpec.describe "Like column", type: :system do
+  fab!(:theme) { upload_theme_component }
+  fab!(:category)
+  fab!(:topics) do
+    Fabricate
+      .times(3, :topic_with_op, category: category)
+      .tap { |topics| topics.each { |t| Fabricate(:like, post: t.first_post) } }
+  end
+
+  it "renders the likes column" do
+    visit "/latest"
+
+    expect(page).to have_css("th.topic-list-data.likes", count: 1)
+    expect(page).to have_css("td.topic-list-data.likes", text: "1", count: 3)
+  end
+end
