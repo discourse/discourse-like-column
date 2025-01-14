d58e6f4e19c69011221947a0b0761d65c86deb29
David Taylor <david@taylorhq.com>
2025-01-14 16:13:13 +0000

    DEV: Update for glimmer topic-list (#26)

--- javascripts/discourse/api-initializers/modify-topic-list.js
+++ javascripts/discourse/api-initializers/modify-topic-list.js
@@ -1,7 +1,29 @@
+import HeaderLikesCell from "discourse/components/topic-list/header/likes-cell";
+import ItemLikesCell from "discourse/components/topic-list/item/likes-cell";
 import { apiInitializer } from "discourse/lib/api";
+import { withSilencedDeprecations } from "discourse-common/lib/deprecated";
 
 export default apiInitializer("0.8", (api) => {
-  api.modifyClass("component:topic-list", {
-    showLikes: true,
+  api.registerValueTransformer("topic-list-columns", ({ value: columns }) => {
+    if (!columns.has("likes")) {
+      columns.add(
+        "likes",
+        {
+          header: HeaderLikesCell,
+          item: ItemLikesCell,
+        },
+        {
+          after: "replies",
+          before: "views",
+        }
+      );
+    }
+    return columns;
+  });
+
+  withSilencedDeprecations("discourse.hbr-topic-list-overrides", () => {
+    api.modifyClass("component:topic-list", {
+      showLikes: true,
+    });
   });
 });
